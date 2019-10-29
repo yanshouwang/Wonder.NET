9d02e911edc1ec0e701b0d22e00106e4750390eb
iDotNET <yanshouwang@outlook.com>
2019-10-30 00:21:54 +0800

    添加 Wonder.Core 项目 添加 ControlWindowBehavior 行为

---
+++ Wonder.Core/Behaviors/ControlWindowBehavior.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Xaml.Behaviors;
+using System.Windows;
+using System.Windows.Input;
+
+namespace Wonder.Core.Behaviors
+{
+    public class ControlWindowBehavior : Behavior<UIElement>
+    {
+        protected override void OnAttached()
+        {
+            base.OnAttached();
+            AssociatedObject.MouseMove += OnMouseMove;
+            AssociatedObject.MouseLeftButtonDown += OnMouseLeftButtonDown;
+        }
+
+        protected override void OnDetaching()
+        {
+            AssociatedObject.MouseMove -= OnMouseMove;
+            AssociatedObject.MouseLeftButtonDown -= OnMouseLeftButtonDown;
+            base.OnDetaching();
+        }
+
+        private void OnMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ClickCount != 2 || !(Window.GetWindow(AssociatedObject) is Window window))
+                return;
+
+            window.WindowState = window.WindowState == WindowState.Normal
+                                ? WindowState.Maximized
+                                : WindowState.Normal;
+        }
+
+        private void OnMouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.LeftButton != MouseButtonState.Pressed || !(Window.GetWindow(AssociatedObject) is Window window))
+                return;
+
+            window.DragMove();
+        }
+    }
+}
