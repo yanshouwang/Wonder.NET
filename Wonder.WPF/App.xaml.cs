a4e2d48d7c735796630cd87b347c10ef26189bab
iDotNET <yanshouwang@outlook.com>
2019-10-29 22:16:32 +0800

    添加项目文件。

---
+++ Wonder.WPF/App.xaml.cs
@@ -0,0 +1,22 @@
+﻿using Prism.Ioc;
+using Wonder.WPF.Views;
+using System.Windows;
+
+namespace Wonder.WPF
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App
+    {
+        protected override Window CreateShell()
+        {
+            return Container.Resolve<MainWindow>();
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+
+        }
+    }
+}
