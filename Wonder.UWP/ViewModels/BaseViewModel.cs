478805fc007b4a91f460824155cb7bdee6548a9e
iDotNET <yanshouwang@outlook.com>
2019-11-05 16:30:32 +0800

    添加 UWP 项目

---
+++ Wonder.UWP/ViewModels/BaseViewModel.cs
@@ -0,0 +1,15 @@
+﻿using Prism.Windows.Mvvm;
+using Prism.Windows.Navigation;
+
+namespace Wonder.UWP.ViewModels
+{
+    public class BaseViewModel : ViewModelBase
+    {
+        protected INavigationService NavigationService { get; }
+
+        public BaseViewModel(INavigationService navigationService)
+        {
+            NavigationService = navigationService;
+        }
+    }
+}
