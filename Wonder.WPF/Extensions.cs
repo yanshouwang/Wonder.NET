98f0113e4681dc4169abe73ae8ae0f101b5f6c08
iDotNET <yanshouwang@outlook.com>
2020-02-28 10:38:39 +0800

    项目搭建

---
+++ Wonder.WPF/Extensions.cs
@@ -0,0 +1,18 @@
+﻿using System.Linq;
+using System.Windows;
+
+namespace Wonder.WPF
+{
+    public static class Extensions
+    {
+        public static Window GetWindow(this UIElement element)
+        {
+            var window = element is Window w1
+                       ? w1
+                       : Window.GetWindow(element) is Window w2
+                       ? w2
+                       : null;
+            return window;
+        }
+    }
+}
